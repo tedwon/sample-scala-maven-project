2a4fc8e4ebc5518d1fdaeff5f0e8209afbe8a12a
tedwon <tedwon@daumcorp.com>
2014-09-23 17:50:45 +0900

    Add Scalatest sample

--- src/main/scala/com/realtimecep/hello.scala
+++ src/main/scala/com/realtimecep/hello.scala
@@ -1,10 +1,7 @@
 package com.realtimecep
 
-/**
- * Created by ted on 7/22/14.
- */
 object HelloWorld {
-  def main(args: Array[String]) {
-    println("Hello, tedwon!!")
+  def main(args: Array[String]): Unit = {
+    println("Hello, scala!")
   }
 }
